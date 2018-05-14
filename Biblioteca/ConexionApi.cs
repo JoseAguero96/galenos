a05f005ee61568b12bda38026ee0a4cc96e96ce7
victoriosu <seb.echeverria@alumnos.duoc.cl>
2018-05-13 22:43:54 -0400

    Creado metodo en biblioteca para consultar centralizadamente

---
+++ Biblioteca/ConexionApi.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Biblioteca
+{
+    public class ConexionApi
+    {
+        public String ejecutarLlamada(String metodo, String a_consultar, String contenido, Object objeto)
+        {
+            String url = "http://apigalenos.herokuapp.com/";
+            String responseString = "";
+            HttpResponseMessage response;
+            HttpClient http = new HttpClient();
+            
+
+
+            switch (metodo)
+            {
+                case "POST":
+                    //necesita objeto
+                    var jsonpost = new JavaScriptSerializer().Serialize(objeto);
+                    StringContent queryString = new StringContent(jsonpost, Encoding.UTF8, "application/json");
+
+                    response = http.PostAsync(url + a_consultar, queryString).Result;
+                    var responseContent = response.Content;
+                    responseString = responseContent.ReadAsStringAsync().Result;
+                    return responseString;
+
+                case "GET":
+                    //para todos es el nombre del modelo en plural
+                    //para uno es lo mismo que el anterior agregandole el id del consultado EJ: "medicos/1
+                    response = http.GetAsync(url + a_consultar).Result;
+                    responseContent = response.Content;
+                    responseString = responseContent.ReadAsStringAsync().Result;
+                    return responseString;
+
+                case "PUT":
+                    //necesita objeto
+                    var jsonput = new JavaScriptSerializer().Serialize(objeto);
+                    var httpContent = new StringContent(jsonput, Encoding.UTF8, "application/json");
+                    response = http.PutAsync(url + a_consultar, httpContent).Result;
+                    responseContent = response.Content;
+                    responseString = responseContent.ReadAsStringAsync().Result;
+                    return responseString;
+
+                case "DELETE":
+                    //lo mismo que el GET de a 1
+                    response = http.DeleteAsync(url + a_consultar).Result;
+                    responseContent = response.Content;
+                    responseString = responseContent.ReadAsStringAsync().Result;
+                    return responseString;
+
+                default:
+                    return "Nada";
+            }
+        }
+    }
+}
